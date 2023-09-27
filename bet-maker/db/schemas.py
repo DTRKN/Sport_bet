a01dfe0c3a6215cba13606c3b735e9fb8d160a15
dens <dens@mail.ru>
2023-09-27 19:14:55 +0500

    small fix

--- bet-maker/db/schemas.py
+++ bet-maker/db/schemas.py
@@ -6,9 +6,9 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     id: str
-    bet: int
-    event_id: str
     time: int
+
+class User_bet(BaseModel):
 
 users: dict[str, User] = {'ivan': User(id='1', bet=5000, event_id='3', time=int(time.time())),
                           'dima': User(id='2', bet=400, event_id='1', time=int(time.time()) + 90),
--- line-prov/db/schemas.py
+++ line-prov/db/schemas.py
@@ -8,7 +8,6 @@ class EventState(enum.Enum):
     NEW = 1
     FINISHED_WIN = 2
     FINISHED_LOSE = 3
-
 class Event(BaseModel):
     event_id: str
     coefficient: Optional[decimal.Decimal] = None
